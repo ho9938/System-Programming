e4e1307c3a5f55f0e0f094076f87febe45fd5595
Yoon Changho <ho9938@unist.ac.kr>
2022-04-28 21:05:47 +0900

    Data Lab: Manipulating Bits: add my name and student id

--- datalab-handout/bits.c
+++ datalab-handout/bits.c
@@ -1,7 +1,7 @@
 /* 
  * CS:APP Data Lab 
  * 
- * <Please put your name and userid here>
+ * Yoon Changho 20181167
  * 
  * bits.c - Source file with your solutions to the Lab.
  *          This is the file you will hand in to your instructor.
